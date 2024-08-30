16edc3803b8ab2882e3bac1f7b8d189f69ee09d8
pydigunna <132155312+pydigunna@users.noreply.github.com>
2024-08-30 22:16:26 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,17 @@
+const increase=document.getElementById("increase");
+const decrease=document.getElementById("decrease");
+const reset=document.getElementById("reset");
+const counter=document.getElementById("counter");
+let count=0;
+increase.onclick=function(){
+    count++;
+    counter.textContent=count;
+}
+decrease.onclick=function(){
+    count--;
+    counter.textContent=count;
+}
+reset.onclick=function(){
+    count=0;
+    counter.textContent=count;
+}
